7276100368af9a9577d1106316ca9c6fcf397665
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-10-09 10:44:49 +0530

    navbar initiated

--- analyst/src/components/navbar/Navbar.jsx
+++ analyst/src/components/navbar/Navbar.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 const Navbar = () => {
-    return <div className="home">Navbar</div>;
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder="Search..." />
+                    <SearchOutlinedIcon />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageOutlinedIcon className="icon" />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <FullscreenExitOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlinedIcon className="icon" />
+                        <div className="counter">1</div>
+                    </div>
+                    <div className="item">
+                        <ChatBubbleOutlineOutlinedIcon className="icon" />
+                        <div className="counter">2</div>
+                    </div>
+                    <div className="item">
+                        <ListOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <img
+                            src="https://a.storyblok.com/f/67418/1672x1018/6463a5af0d/screenshot-2022-05-24-at-15-22-28.png"
+                            className="avatar"
+                            alt=""
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
