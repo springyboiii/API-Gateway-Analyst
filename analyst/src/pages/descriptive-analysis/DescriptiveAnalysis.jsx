1e42a568047bb20514cf7c2c9f6583a970b37d03
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-11-04 00:18:05 +0530

    added draft of descriptive analysis page

--- analyst/src/pages/descriptive-analysis/DescriptiveAnalysis.jsx
+++ analyst/src/pages/descriptive-analysis/DescriptiveAnalysis.jsx
@@ -9,7 +9,20 @@ const DescriptiveAnalysis = () => {
             <Sidebar />
             <div className="descriptiveAnalysisContainer">
                 <Navbar />
-                <p>descriptive analysis</p>
+
+                <div className="dashboardBody">
+                    <h1 className="title">Descriptive Analysis</h1>
+                    <div className="dashboard">
+                        <iframe
+                            title="graphs - Page 3"
+                            width="1000"
+                            height="700"
+                            src="https://app.powerbi.com/view?r=eyJrIjoiOTk3MTJkNjYtOGEzNi00OWQzLTk0ZGYtZTk0Mjk3NGUwZjU4IiwidCI6ImFhYzBjNTY0LTZjNWUtNGIwNS04ZGMzLTQwODA4N2Y3N2Y3NiIsImMiOjEwfQ%3D%3D"
+                            frameborder="0"
+                            allowFullScreen="true"
+                        ></iframe>
+                    </div>
+                </div>
             </div>
         </div>
     );
