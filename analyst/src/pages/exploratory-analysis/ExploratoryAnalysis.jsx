9b01e440ad7800cbc72ec8409b6b6968b0cb7a85
springyboiii <sugumararvinth19991018@gmail.com>
2022-11-15 20:35:40 +0530

    test type=0 count

--- analyst/src/pages/exploratory-analysis/ExploratoryAnalysis.jsx
+++ analyst/src/pages/exploratory-analysis/ExploratoryAnalysis.jsx
@@ -138,10 +138,11 @@ const ExploratoryAnalysis = () => {
      })
      .then((response) => {
        console.log(response)
+       console.log("response")
        const res =response.data
        setProfileData(({
-         profile_name: res.name,
-         about_me: res.about}))
+         profile_name: res.count,
+         }))
      }).catch((error) => {
        if (error.response) {
          console.log(error.response)
@@ -192,29 +193,15 @@ const ExploratoryAnalysis = () => {
         
 //       </div>
       <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
 
         {/* new line start*/}
         <p>To get your profile details: </p><button onClick={getData}>Click me</button>
         {profileData && <div>
-              <p>Profile name: {profileData.profile_name}</p>
-              <p>About me: {profileData.about_me}</p>
+              <p>Count: {profileData.profile_name}</p>
             </div>
         }
          {/* end of new line */}
-      </header>
     </div>
     // </div>
   );
--- server/server.py
+++ server/server.py
@@ -5,11 +5,12 @@ import pymongo
 
 from controllers.predict import PredictController
 from controllers.data import DataController
-
+import certifi
+ca= certifi.where()
 app = Flask(__name__)
 # don't hardcode passsword
-mongo = pymongo.MongoClient("mongodb+srv://chathuranga123:chathuranga123@apigatewayanalystcluste.lz68ckp.mongodb.net/?retryWrites=true&w=majority")
 
+mongo = pymongo.MongoClient("mongodb+srv://chathuranga123:chathuranga123@apigatewayanalystcluste.lz68ckp.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=ca)
 CORS(app)
 
 db = mongo["api_gateway_analyst"]
@@ -37,13 +38,19 @@ def getPreprocessedData():
 
 @app.route('/profile', methods=["GET"])
 def my_profile():
-    print("backend profileeeeeeeeeeeeeeeeeeeeeeee")
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
-
-    return response_body
+    # print("backend profileeeeeeeeeeeeeeeeeeeeeeee")
+    # response_body = {
+    #     "name": "Nagato",
+    #     "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    # }
+    # result=col.find_one({})
+    count=0
+    for x in col.find({},{ "type": 0 }):
+        # print(x)
+        count+=1
+    result={"count":count}
+    print(result,"Counttttttttttttttttttttttttttt")
+    return result
 
 if __name__ == "__main__":
     print("Starting Python Flask Server for API Gateway Analyst")
