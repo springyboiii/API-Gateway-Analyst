63548e8a3c1ec0654f9813705c936f20dd222bc7
springyboiii <sugumararvinth19991018@gmail.com>
2022-11-15 10:16:36 +0530

    adding api connection

--- server/server.py
+++ server/server.py
@@ -5,34 +5,33 @@ import pymongo
 
 from controllers.predict import PredictController
 from controllers.data import DataController
-
+import certifi
+ca = certifi.where()
 app = Flask(__name__)
 # don't hardcode passsword
-mongo = pymongo.MongoClient("mongodb+srv://chathuranga123:chathuranga123@apigatewayanalystcluste.lz68ckp.mongodb.net/?retryWrites=true&w=majority")
+mongo = pymongo.MongoClient("mongodb+srv://chathuranga123:chathuranga123@apigatewayanalystcluste.lz68ckp.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
 
 CORS(app)
 
 db = mongo["api_gateway_analyst"]
-col = db["test_cpu"]
+col = db["preprocessed_10_sec"]
 
 @app.route('/', methods=["GET"])
 def init():
     print(col.find_one())
-    # print(col.find_one())
-    print("ASDasdasdasdas")
     return "hi"
 
 @app.route('/predict', methods=["GET"])
 def predict():
     pass
 
-# @app.route('/test', methods=["GET"])
-# def getTestData():
-#     return PredictController.getTestCpuData(db)
+@app.route('/test', methods=["GET"])
+def getTestData():
+    return PredictController.getTestCpuData(db)
 
-# @app.route('/preprocessed', methods=["GET"])
-# def getPreprocessedData():
-#     return DataController.getPreprocessedData(db)
+@app.route('/preprocessed', methods=["GET"])
+def getPreprocessedData():
+    return DataController.getPreprocessedData(db)
 
 if __name__ == "__main__":
     print("Starting Python Flask Server for API Gateway Analyst")
