740ccfe82cddfe06a2dde48137f18ca9d6893e07
springyboiii <sugumararvinth19991018@gmail.com>
2022-11-16 13:58:49 +0530

    polished the graphs

--- server/server.py
+++ server/server.py
@@ -36,7 +36,7 @@ def getPreprocessedDataCount():
 def getPreprocessedData():
     return DataController.getPreprocessedData(db)
 
-@app.route('/normal_anomaly_doughnut', methods=["GET"])
+@app.route('/normal_anomaly_doughnut_data', methods=["GET"])
 def normal_anomaly_doughnut():
     # total_count=col.count_documents(filter={})
     normal_count=col.count_documents({"type":0})
@@ -48,6 +48,50 @@ def normal_anomaly_doughnut():
     #     anomaly_count+=len(list(col.find({"type":type})))
     
     result={"normal":normal_count,"anomaly":total_count-normal_count}
+    return result
+
+@app.route('/anomaly_type_doughnut_data', methods=["GET"])
+def anomaly_type_doughnut():
+    result=dict()
+    for type in range(1,8):
+        result["type"+str(type)]=col.count_documents({"type":type})
+    # print(result)
+    return result
+
+@app.route('/scenario_doughnut_data', methods=["GET"])
+def scenario_doughnut():
+
+    result=dict()
+    for scenario in range(1,11):
+        result["scenario"+str(scenario)]=col.count_documents({"scenario":scenario})
+    # result={"normal":normal_count,"anomaly":total_count-normal_count}
+    # print(result)
+    return result
+
+@app.route('/jvm_metrics_memory_heap_memory_usage_used_data', methods=["GET"])
+def jvm_metrics_memory_heap_memory_usage_used():
+
+    
+    jvm_metrics_memory_heap_memory_usage_used_count=col.count_documents({"jvm_metrics_memory_heap_memory_usage_used":{"$gt": 1.2}})
+    total_count=col.estimated_document_count()
+
+    result={"jvm_memory":jvm_metrics_memory_heap_memory_usage_used_count,"normal":total_count-jvm_metrics_memory_heap_memory_usage_used_count}
+    return result
+
+@app.route('/anomaly_time_area_data', methods=["GET"])
+def anomaly_time_area_data():
+    result=dict()
+    result["timestamp"]=[]
+    result["system_cpu_user_pct"]=[]
+    result["type"]=[]
+    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
+        # print(x["timestamp"])
+        result["timestamp"].append(x["timestamp"])
+        result["system_cpu_user_pct"].append(x["system_cpu_user_pct"])
+        result["type"].append(x["type"])
+    # print(result["type"])
+
+    
     return result
 
 if __name__ == "__main__":
