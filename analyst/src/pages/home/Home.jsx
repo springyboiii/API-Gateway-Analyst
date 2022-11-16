f66ba6506a3f8bd9abf11a96a461037f21f537f6
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-11-16 11:15:07 +0530

    print only 1.. after one

--- analyst/src/pages/home/Home.jsx
+++ analyst/src/pages/home/Home.jsx
@@ -9,47 +9,50 @@ import { useState, useEffect } from "react";
 let socket;
 
 const Home = () => {
-    const [msgList, setMsgList] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-    const sendMessage = (e) => {
-        // e.preventDefault();
-        socket.emit("sendMsg", { message: "hello" });
+  const sendMessage = (e) => {
+    // e.preventDefault();
+    socket.emit("sendMsg", { message: "hello" });
+  };
+
+  useEffect(() => {
+    socket = io.connect("http://127.0.0.1:5000/");
+    socket.on("connect", function () {
+      socket.send("a");
+    });
+
+    socket.on("recvMsg", (data) => {
+      setMessages([...messages, data]);
+      console.log(messages);
+    });
+
+    return () => {
+      socket.off("data", () => {
+        console.log("data event was removed");
+      });
     };
+  }, [socket, messages]);
 
-    useEffect(() => {
-        socket = io.connect("http://127.0.0.1:5000/");
-        socket.on("connect", function () {
-            socket.send("a");
-        });
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        Main Dashboard
+        <div className="test">
+          <input type="text" placeholder="message" />
+          <button onClick={sendMessage}>Send message</button>
 
-        socket.on("recvMsg", (data) => {
-            console.log(data);
-            // setMsgList((lst) => [...lst, data]);
-            // console.log(msgList);
-        });
-
-        // socket.on("message", (data) => {
-        //     console.log(data);
-        //     // setMsgList((lst) => [...lst, data]);
-        //     // console.log(msgList);
-        // });
-    }, []);
-
-    return (
-        <div className="home">
-            <Sidebar />
-            <div className="homeContainer">
-                <Navbar />
-                Main Dashboard
-                <div className="test">
-                    <input type="text" placeholder="message" />
-                    <button onClick={sendMessage}>Send message</button>
-
-                    {/* {msgList} */}
-                </div>
-            </div>
+          <ul>
+            {messages.map((message, index) => {
+              return <li key={index}>{message}</li>;
+            })}
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- server/thread.py
+++ server/thread.py
@@ -15,6 +15,7 @@ from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='bruh'
+# app.config['DEBUG']=False
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 CORS(app)
@@ -45,43 +46,27 @@ def sendMsg(msg):
     global i 
     global connections
     global socketio
-    
+    global somelist
+
+    print("before")
+    # for thread in threading.enumerate(): 
+    #     print(thread.name)
     for i in range(10):
         # print("inside thread ....")
-        # if (1 > 0):
-        print("connections are there")
-        socketio.emit("recvMsg",somelist[i % len(somelist)], broadcast=True)
-        time.sleep(2)
-
-# @socketio.on("message")
-# def handleMessage(msg):
-#     global sema 
-
-#     # sema.acquire()
-#     for i in range(10):
-#         # print("inside thread ....")
-#         # if (1 > 0):
-#         print(f"i = {i}...")
-#         # print(f"active threads = {threading.active_count()}")
-#         print(f"thread name = {current_thread().name}")
-#         send(somelist[i % len(somelist)], broadcast=True)
-#         time.sleep(2)
-    # sema.release()
+        if (connections > 0):
+            socketio.emit("recvMsg", str(i), broadcast=True)
+            time.sleep(2)
+    print("after")
+    for thread in threading.enumerate(): 
+        print(thread.name)
 
 thread1 = threading.Thread(target=sendMsg, args=("I",))
 thread1.start()
 
-# def printline(msg):
-#     for i in range(5):
-#         print("thread working")
-#         time.sleep(2)
-
-# thread2 = threading.Thread(target=printline, args=("I",))
-# thread2.start()
-
 if __name__ == "__main__":
     print("Starting Python Flask Server for API Gateway Analyst")
 
-    socketio.run(app)
+
+    socketio.run(app, debug=False)
     
     
