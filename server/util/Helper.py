d21fb855a82924ece907b9a82445c6b1e7fb3037
chathuranga-jayanath-99 <chathuranga.19@cse.mrt.ac.lk.com>
2022-11-19 21:04:30 +0530

    scaler addede to artifacts

--- server/util/Helper.py
+++ server/util/Helper.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np 
 
 import os
+from datetime import datetime, timedelta
+from pickle import load
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,23 +15,23 @@ class Helper():
     def orderDict(dataDict):
         orderedDict = {}
         colsOrder = ['system_cpu_user_pct', 'system_cpu_system_pct',
-       'system_cpu_idle_pct', 'system_cpu_iowait_pct',
-       'system_cpu_softirq_pct', 'system_cpu_total_pct',
-       'system_memory_used_pct', 'system_network_in_bytes',
-       'system_network_in_packets', 'system_network_in_dropped',
-       'system_network_out_bytes', 'system_network_out_packets',
-       'system_network_out_errors',
-       'jvm_metrics_memory_heap_memory_usage_committed',
-       'jvm_metrics_memory_heap_memory_usage_max',
-       'jvm_metrics_memory_heap_memory_usage_used',
-       'jvm_metrics_threading_thread_count',
-       'jvm_metrics_gc_psms_collection_count',
-       'jvm_metrics_gc_psms_collection_time',
-       'jvm_metrics_gc_pss_collection_count',
-       'jvm_metrics_gc_pss_collection_time', 'system_diskio_iostat_await',
-       'system_diskio_iostat_queue_avg_size',
-       'system_diskio_iostat_read_per_sec_bytes',
-       'system_diskio_iostat_write_per_sec_bytes']
+        'system_cpu_idle_pct', 'system_cpu_iowait_pct',
+        'system_cpu_softirq_pct', 'system_cpu_total_pct',
+        'system_memory_used_pct', 'system_network_in_bytes',
+        'system_network_in_packets', 'system_network_in_dropped',
+        'system_network_out_bytes', 'system_network_out_packets',
+        'system_network_out_errors',
+        'jvm_metrics_memory_heap_memory_usage_committed',
+        'jvm_metrics_memory_heap_memory_usage_max',
+        'jvm_metrics_memory_heap_memory_usage_used',
+        'jvm_metrics_threading_thread_count',
+        'jvm_metrics_gc_psms_collection_count',
+        'jvm_metrics_gc_psms_collection_time',
+        'jvm_metrics_gc_pss_collection_count',
+        'jvm_metrics_gc_pss_collection_time', 'system_diskio_iostat_await',
+        'system_diskio_iostat_queue_avg_size',
+        'system_diskio_iostat_read_per_sec_bytes',
+        'system_diskio_iostat_write_per_sec_bytes']
 
         for col in colsOrder:
             orderedDict[col] = dataDict[col]
@@ -41,17 +44,32 @@ class Helper():
             temp = [dataDict[key]]
             dataDict[key] = temp 
         
-
-
         return pd.DataFrame.from_dict(dataDict)
     
     def getIndexOfMax(npArr):
         if (npArr is None): return None
-        return np.argmax(npArr)
+        return [np.argmax(i) for i in npArr]
     
     def loadModel():
         loadedModel = keras.models.load_model(os.getenv("SERVER_PATH")+"artifacts/ann_model.h5")
         return loadedModel
+    
+    def loadMinMaxScaler():
+        loadMinMaxScaler = load(open(os.getenv("SERVER_PATH")+"artifacts/minmax_scaler.pkl", 'rb'))
+        return loadMinMaxScaler
+
+    def convertStrToDatetime(timeStr):
+      # eg: 2018-01-06 18:54:00
+      datetimeObj = datetime.strptime(timeStr, "%Y-%m-%d %H:%M:%S")
+      return datetimeObj
+    
+    def getNextTimestamp(timestamp):
+      # timestamp is in datetime format 
+      delta = timedelta(seconds=10)
+      return timestamp + delta 
+
+
+
 
 if __name__ == '__main__':
     # print(Helper.loadModel())
@@ -134,6 +152,9 @@ if __name__ == '__main__':
   ]
 }
 
-    res = Helper.orderDict(temp)
-    print(res)
-    print("Done")
+    # res = Helper.orderDict(temp)
+    # print(res)
+    # print("Done")
+
+    dateObj = Helper.convertStrToDatetime("2018-01-06 18:54:00")
+    print(dateObj)
