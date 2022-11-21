402c04c858dec9e37bff2bb9c2e5388786cc5feb
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2022-11-21 18:17:40 +0530

    discrip clean

---
+++ server/controllers/dashboard.py
@@ -0,0 +1,73 @@
+from flask import jsonify
+from flask_pymongo import PyMongo, ObjectId
+import pymongo
+import random
+import numpy as np
+
+class DashboardController():
+    def normal_anomaly_doughnut(col):
+        normal_count = col.count_documents({"type": 0})
+
+        total_count = col.estimated_document_count()
+
+        result = {"normal": normal_count, "anomaly": total_count-normal_count}
+        return result
+
+    def anomaly_type_doughnut(col):
+        result = dict()
+        for type in range(1, 8):
+            result["type"+str(type)] = col.count_documents({"type": type})
+        return result
+
+    def scenario_doughnut(col):
+
+        result = dict()
+        for scenario in range(1, 11):
+            result["scenario" +
+                   str(scenario)] = col.count_documents({"scenario": scenario})
+
+        return result
+
+    def jvm_metrics_memory_heap_memory_usage_used(col):
+
+        jvm_metrics_memory_heap_memory_usage_used_count = col.count_documents(
+            {"jvm_metrics_memory_heap_memory_usage_used": {"$gt": 1.2}})
+        total_count = col.estimated_document_count()
+
+        result = {"jvm_memory": jvm_metrics_memory_heap_memory_usage_used_count,
+                  "normal": total_count-jvm_metrics_memory_heap_memory_usage_used_count}
+        return result
+
+    def get_recent_line_graph(col, feature, limit):
+        result = dict()
+        result["timestamp"] = []
+        result[feature] = []
+        result["type"] = []
+        for x in col.find().limit(limit).sort("timestamp", pymongo.DESCENDING):
+            result["timestamp"].append(x["timestamp"][11:])
+            result[feature].append(x[feature])
+            result["type"].append(x["type"])
+        return result
+
+    def get_prediction_bar_graph(col, limit):
+        result = dict()
+        result["timestamp"] = []
+        result["prediction"] = []
+        result["bgcolor"]=[]
+        for x in col.find().limit(limit).sort("timestamp", pymongo.DESCENDING):
+            result["timestamp"].append(x["timestamp"][11:])
+            # if x["type"]==0:
+            #     result["prediction"].append(0)
+            # else:
+            #     result["prediction"].append(1)
+        # print([random.choice(range(2)) for i in range(1000)])
+        result["prediction"] = [random.choice(range(2)) for i in range(1000)]
+        result["dummy"]=[1 for x in range(1000)]
+        green="rgba(54, 162, 235, 1)";
+        red="rgba(255, 99, 132, 1)"
+        for x in result["prediction"]:
+            if x==0:
+                result["bgcolor"].append(green)
+            else:
+                result["bgcolor"].append(red)
+        return result
--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import pymongo
 
 from controllers.predict import PredictController
 from controllers.data import DataController
+from controllers.dashboard import DashboardController
 import certifi
 ca= certifi.where()
 app = Flask(__name__)
@@ -38,350 +39,134 @@ def getPreprocessedData():
 
 @app.route('/normal_anomaly_doughnut_data', methods=["GET"])
 def normal_anomaly_doughnut():
-    # total_count=col.count_documents(filter={})
-    normal_count=col.count_documents({"type":0})
-
-    total_count=col.estimated_document_count()
-
-    # anomaly_count=0
-    # for type in range(1,8):
-    #     anomaly_count+=len(list(col.find({"type":type})))
-    
-    result={"normal":normal_count,"anomaly":total_count-normal_count}
-    return result
+    return DashboardController.normal_anomaly_doughnut(col)
 
 @app.route('/anomaly_type_doughnut_data', methods=["GET"])
 def anomaly_type_doughnut():
-    result=dict()
-    for type in range(1,8):
-        result["type"+str(type)]=col.count_documents({"type":type})
-    # print(result)
-    return result
+    return DashboardController.anomaly_type_doughnut(col)
 
 @app.route('/scenario_doughnut_data', methods=["GET"])
 def scenario_doughnut():
-
-    result=dict()
-    for scenario in range(1,11):
-        result["scenario"+str(scenario)]=col.count_documents({"scenario":scenario})
-    # result={"normal":normal_count,"anomaly":total_count-normal_count}
-    # print(result)
-    return result
-
+    return DashboardController.scenario_doughnut(col)
+#need to change graph
 @app.route('/jvm_metrics_memory_heap_memory_usage_used_data', methods=["GET"])
 def jvm_metrics_memory_heap_memory_usage_used():
-
-    
-    jvm_metrics_memory_heap_memory_usage_used_count=col.count_documents({"jvm_metrics_memory_heap_memory_usage_used":{"$gt": 1.2}})
-    total_count=col.estimated_document_count()
-
-    result={"jvm_memory":jvm_metrics_memory_heap_memory_usage_used_count,"normal":total_count-jvm_metrics_memory_heap_memory_usage_used_count}
-    return result
+    return DashboardController.jvm_metrics_memory_heap_memory_usage_used(col)
 
 @app.route('/anomaly_time_area_data', methods=["GET"])
 def anomaly_time_area_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_user_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        # print(x["timestamp"])
-        result["timestamp"].append(x["timestamp"])
-        result["system_cpu_user_pct"].append(x["system_cpu_user_pct"])
-        result["type"].append(x["type"])
-    # print(result["type"])
-
-    
-    return result
+    return DashboardController.get_recent_line_graph(col,"system_cpu_user_pct",1000)
 
 # cpu
 @app.route('/user_pct_data', methods=["GET"])
 def user_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_user_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_cpu_user_pct"].append(x["system_cpu_user_pct"])
-        result["type"].append(x["type"])
-    return result
-
+    return DashboardController.get_recent_line_graph(col,"system_cpu_user_pct",1000)
 
 @app.route('/system_pct_data', methods=["GET"])
 def system_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_system_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_cpu_system_pct"].append(x["system_cpu_system_pct"])
-        result["type"].append(x["type"])
-    return result
-
+    return DashboardController.get_recent_line_graph(col,"system_cpu_system_pct",1000)
 
 @app.route('/idle_pct_data', methods=["GET"])
 def idle_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_idle_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_cpu_idle_pct"].append(x["system_cpu_idle_pct"])
-        result["type"].append(x["type"])
-    return result
-
+    return DashboardController.get_recent_line_graph(col,"system_cpu_idle_pct",1000)
 
 @app.route('/iowait_pct_data', methods=["GET"])
 def iowait_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_iowait_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_cpu_iowait_pct"].append(x["system_cpu_iowait_pct"])
-        result["type"].append(x["type"])
-    return result
-
+    return DashboardController.get_recent_line_graph(col,"system_cpu_iowait_pct",1000)
 
 @app.route('/softirq_pct_data', methods=["GET"])
 def softirq_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_softirq_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_cpu_softirq_pct"].append(x["system_cpu_softirq_pct"])
-        result["type"].append(x["type"])
-    return result
-
+    return DashboardController.get_recent_line_graph(col,"system_cpu_softirq_pct",1000)
 
 @app.route('/total_pct_data', methods=["GET"])
 def total_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_cpu_total_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_cpu_total_pct"].append(x["system_cpu_total_pct"])
-        result["type"].append(x["type"])
-    return result   
+    return DashboardController.get_recent_line_graph(col,"system_cpu_total_pct",1000)
+
+@app.route('/prediction_bar_data', methods=["GET"])
+def prediction_bar_data():
+    return DashboardController.get_prediction_bar_graph(col,50)
 
 
 # memory
 @app.route('/memory_used_pct', methods=["GET"])
 def memory_used_pct_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_memory_used_pct"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_memory_used_pct"].append(x["system_memory_used_pct"])
-        result["type"].append(x["type"])
-    return result 
+    return DashboardController.get_recent_line_graph(col,"system_memory_used_pct",1000)
 
 
 # disk
 # system_diskio_iostat_await
 @app.route('/diskio_iostat_await', methods=["GET"])
 def diskio_iostat_await_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_diskio_iostat_await"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_diskio_iostat_await"].append(x["system_diskio_iostat_await"])
-        result["type"].append(x["type"])
-    return result 
+    return DashboardController.get_recent_line_graph(col,"system_diskio_iostat_await",1000)
 
 # system_diskio_iostat_queue_avg_size
 @app.route('/diskio_iostat_queue_avg_size', methods=["GET"])
 def diskio_iostat_queue_avg_size_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_diskio_iostat_queue_avg_size"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_diskio_iostat_queue_avg_size"].append(x["system_diskio_iostat_queue_avg_size"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_diskio_iostat_queue_avg_size",1000)
 
 # system_diskio_iostat_read_per_sec_bytes
 @app.route('/diskio_iostat_read_per_sec_bytes', methods=["GET"])
 def diskio_iostat_read_per_sec_bytes_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_diskio_iostat_read_per_sec_bytes"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_diskio_iostat_read_per_sec_bytes"].append(x["system_diskio_iostat_read_per_sec_bytes"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_diskio_iostat_read_per_sec_bytes",1000)
 
 # system_diskio_iostat_write_per_sec_bytes
 @app.route('/diskio_iostat_write_per_sec_bytes', methods=["GET"])
 def diskio_iostat_write_per_sec_bytes_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_diskio_iostat_write_per_sec_bytes"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_diskio_iostat_write_per_sec_bytes"].append(x["system_diskio_iostat_write_per_sec_bytes"])
-        result["type"].append(x["type"])
-    return result 
+    return DashboardController.get_recent_line_graph(col,"system_diskio_iostat_write_per_sec_bytes",1000)
 
 
 # jvm
 # jvm_metrics_gc_psms_collection_count
 @app.route('/psms_collection_count', methods=["GET"])
 def psms_collection_count_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["jvm_metrics_gc_psms_collection_count"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["jvm_metrics_gc_psms_collection_count"].append(x["jvm_metrics_gc_psms_collection_count"])
-        result["type"].append(x["type"])
-    return result 
+    return DashboardController.get_recent_line_graph(col,"jvm_metrics_gc_psms_collection_count",1000)
 
 # jvm_metrics_gc_psms_collection_time
 @app.route('/psms_collection_time', methods=["GET"])
 def psms_collection_time_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["jvm_metrics_gc_psms_collection_time"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["jvm_metrics_gc_psms_collection_time"].append(x["jvm_metrics_gc_psms_collection_time"])
-        result["type"].append(x["type"])
-    return result
+    return DashboardController.get_recent_line_graph(col,"jvm_metrics_gc_psms_collection_time",1000)
 
 # jvm_metric_gc_pss_collection_count
 @app.route('/pss_collection_count', methods=["GET"])
 def pss_collection_count_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["jvm_metric_gc_pss_collection_count"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["jvm_metric_gc_pss_collection_count"].append(x["jvm_metric_gc_pss_collection_count"])
-        result["type"].append(x["type"])
-    return result 
+    return DashboardController.get_recent_line_graph(col,"jvm_metric_gc_pss_collection_count",1000)
 
 # jvm_metrics_gc_pss_collection_time
 @app.route('/pss_collection_time', methods=["GET"])
 def pss_collection_time_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["jvm_metrics_gc_pss_collection_time"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["jvm_metrics_gc_pss_collection_time"].append(x["jvm_metrics_gc_pss_collection_time"])
-        result["type"].append(x["type"])
-    return result 
+    return DashboardController.get_recent_line_graph(col,"jvm_metrics_gc_pss_collection_time",1000)
 
 
 # network
 # system_network_in_bytes
 @app.route('/network_in_bytes', methods=["GET"])
 def network_in_bytes_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_network_in_bytes"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_network_in_bytes"].append(x["system_network_in_bytes"])
-        result["type"].append(x["type"])
-    return result 
-
-
+    return DashboardController.get_recent_line_graph(col,"system_network_in_bytes",1000)
 
 # system_network_in_packets
 @app.route('/network_in_packets', methods=["GET"])
 def network_in_packets_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_network_in_packets"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_network_in_packets"].append(x["system_network_in_packets"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_network_in_packets",1000)
 
 # system_network_in_dropped
 @app.route('/network_in_dropped', methods=["GET"])
 def network_in_dropped_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_network_in_dropped"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_network_in_dropped"].append(x["system_network_in_dropped"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_network_in_dropped",1000)
 
 # system_network_out_bytes
 @app.route('/network_out_bytes', methods=["GET"])
 def network_out_bytes_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_network_out_bytes"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_network_out_bytes"].append(x["system_network_out_bytes"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_network_out_bytes",1000)
 
 # system_network_out_packets
 @app.route('/network_out_packets', methods=["GET"])
 def network_out_packets_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_network_out_packets"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_network_out_packets"].append(x["system_network_out_packets"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_network_out_packets",1000)
 
 # system_network_out_errors
 @app.route('/network_out_errors', methods=["GET"])
 def network_out_errors_data():
-    result=dict()
-    result["timestamp"]=[]
-    result["system_network_out_errors"]=[]
-    result["type"]=[]
-    for x in col.find().limit(1000).sort("timestamp",pymongo.DESCENDING):
-        result["timestamp"].append(x["timestamp"][11:])
-        result["system_network_out_errors"].append(x["system_network_out_errors"])
-        result["type"].append(x["type"])
-    return result 
-
+    return DashboardController.get_recent_line_graph(col,"system_network_out_errors",1000)
 
 if __name__ == "__main__":
     print("Starting Python Flask Server for API Gateway Analyst")
