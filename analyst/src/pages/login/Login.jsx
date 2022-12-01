617e1072d82962d567f32e028cd22e5364f841ce
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2022-12-01 08:40:13 +0530

    Update Login.jsx

--- analyst/src/pages/login/Login.jsx
+++ analyst/src/pages/login/Login.jsx
@@ -8,60 +8,86 @@ const initialState = {
     email: '',
     password: '',
     isMember: true,
-}
-
-const Login = () => {
+  }
+  
+  const Login = () => {
+  
     const [values, setValues] = useState(initialState)
-
+    // golbal state and useNavigate
+  
+    const toggleMember = () => {
+      setValues({ ...values, isMember: !values.isMember })
+    }
+  
     const handleChange = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value })
+      setValues({ ...values, [e.target.name]: e.target.value })
     }
-
+  
     const onSubmit = (e) => {
-        e.preventDefault()
-        const { name, email, password, isMember } = values
-        if (!email || !password || (!name && !isMember)) {
-            console.log('email or password incorrect')
-            return
-        }
-        console.log(values)
-
+      e.preventDefault()
+    
+      const { name, email, password, isMember } = values
+      if (!email || !password || (!name && !isMember)) {
+        return
+      }
+      const currentUser = { name, email, password }
+      console.log(currentUser)
+      if (isMember) {
+        // loginUser(currentUser)
+      }
+      else {
+        // registerUser(currentUser)
+      }
+      // console.log(values)
+  
     }
+    // useEffect(() => {
+    //   if (user) {
+    //     setTimeout(() => {
+    //       navigate('/')
+    //     }, 3000)
+    //   }
+    // }, [user, navigate])
+  
     return (
-        <div className="full-page">
-            <form className="form" onSubmit={onSubmit}>
-                {/* <Logo /> */}
-                {/* <h3>{values.isMember ? "Login" : "Register"}</h3>
-                {showAlert && <Alert />} */}
-                {/* name input */}
+        <form className="form" onSubmit={onSubmit}>
+          <h3>{values.isMember ? "Login" : "Register"}</h3>
+          {/* name input */}
+          {!values.isMember && (
+            <FormRow
+              type="text"
+              name="name"
+              value={values.name}
+              handleChange={handleChange}
+            />
+          )}
+          {/* email input */}
+          <FormRow
+            type="email"
+            name="email"
+            value={values.email}
+            handleChange={handleChange}
 
-                <FormRow
-                    type="text"
-                    name="name"
-                    value={values.name}
-                    handleChange={handleChange}
-                />
+          />
+          {/* password input */}
+          <FormRow
+            type="password"
+            name="password"
+            value={values.password}
+            handleChange={handleChange}
 
-                {/* email input */}
-                <FormRow
-                    type="email"
-                    name="email"
-                    value={values.email}
-                    handleChange={handleChange}
-                />
-                {/* password input */}
-                <FormRow
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    handleChange={handleChange}
-                />
-                <button type="submit" className="btn btn-block">
-                    sumbit
-                </button>
-            </form>
-        </div>
+          />
+          <button type="submit" className="btn btn-block">
+            submit
+          </button>
+          <p>
+            {values.isMember ? "Not a member yet" : "Already a member?"}
+            <button type="button" onClick={toggleMember}
+              className='member-btn'>
+              {values.isMember ? 'Register' : 'Login'}
+            </button>
+          </p>
+        </form>
     )
-}
-
+  }
 export default Login;
