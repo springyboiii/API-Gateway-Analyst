75502c1b97b8a65e6f3b1999908bb6500f4d1253
chathuranga-jayanath-99 <chathuranga.19@cse.mrt.ac.lk>
2022-12-26 17:06:14 +0530

    Constants updated with all anomaly types

--- server/util/Constant.py
+++ server/util/Constant.py
@@ -9,6 +9,15 @@ class Constant:
     def getAnomalyTypes(): 
         anomalyTypes = {
             0: "Not an anomaly",
-            1: "Anomaly detected",
+            1: "Anomaly detected (Due to: High CPU usage due to mediation)",
+            2: "Anomaly detected (Due to: High Memory usage due to mediation)",
+            3: "Anomaly detected (Due to: High disk I/O due to mediation)",
+            4: "Anomaly detected (Due to: Increased load (numbers of users))",
+            5: "Anomaly detected (Due to: Increased load (throughput))",
+            6: "Anomaly detected (Due to: Long response time in back-end services)",
+            7: "Anomaly detected (Due to: Increased message size)",
+            8: "Anomaly detected (Due to: Failure in back-end services)",
+            9: "Anomaly detected (Due to: Increased load (throughput))",
+            10: "Anomaly detected (Due to: Increased message size)",
         }
         return anomalyTypes
