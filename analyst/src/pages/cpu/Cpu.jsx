02d21f1e8bb92a2ff006fa5036b3fadb46183ca3
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2022-12-27 16:41:24 +0530

    charts options

--- analyst/src/pages/cpu/Cpu.jsx
+++ analyst/src/pages/cpu/Cpu.jsx
@@ -27,54 +27,42 @@ ChartJS.register(
   Filler
 );
 function Cpu() {
-  const [user_pct_data, set_user_pct_data] = useState({
-    datasets: [],
-  });
+  const [user_pct_data, set_user_pct_data] = useState({datasets: [],});
   const [user_pct_options, set_user_pct_options] = useState({});
-  
-  const [system_pct_data, set_system_pct_data] = useState({
-    datasets: [],
-  });
+
+  const [system_pct_data, set_system_pct_data] = useState({datasets: [],});
   const [system_pct_options, set_system_pct_options] = useState({});
-  
-  const [idle_pct_data, set_idle_pct_data] = useState({
-    datasets: [],
-  });
+
+  const [idle_pct_data, set_idle_pct_data] = useState({datasets: [],});
   const [idle_pct_options, set_idle_pct_options] = useState({});
-  
-  const [iowait_pct_data, set_iowait_pct_data] = useState({
-    datasets: [],
-  });
+
+  const [iowait_pct_data, set_iowait_pct_data] = useState({datasets: [],});
   const [iowait_pct_options, set_iowait_pct_options] = useState({});
 
-  const [softirq_pct_data, set_softirq_pct_data] = useState({
-    datasets: [],
-  });
+  const [softirq_pct_data, set_softirq_pct_data] = useState({datasets: [],});
   const [softirq_pct_options, set_softirq_pct_options] = useState({});
-  
-  const [total_pct_data, set_total_pct_data] = useState({
-    datasets: [],
-  });
+
+  const [total_pct_data, set_total_pct_data] = useState({datasets: [],});
   const [total_pct_options, set_total_pct_options] = useState({});
-  
+
   useEffect(() => {
     axios({
       method: "GET",
       url: "/user_pct_data",
     })
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         const res = response.data;
         set_user_pct_data({
           labels: res.timestamp,
           datasets: [
             {
-              fill: true,
+              fill: false,
               // label: 'system_cpu_user_pct',
               data: res.system_cpu_user_pct,
               borderColor: "rgb(53, 162, 235)",
-              backgroundColor: "rgba(53, 162, 235, 0.5)",
-              tension: 0.4,
+              // backgroundColor: "rgba(53, 162, 235, 0.5)",
+              // tension: 0.4,
             },
           ],
         });
@@ -95,7 +83,7 @@ function Cpu() {
         },
         title: {
           display: true,
-          text: "System_cpu_user_pct",
+          text: "system_cpu_user_pct",
         },
       },
       scales: {
@@ -155,197 +143,197 @@ function Cpu() {
     });
 
     axios({
-        method: "GET",
-        url: "/idle_pct_data",
-      })
-        .then((response) => {
-          console.log(response);
-          const res = response.data;
-          set_idle_pct_data({
-            labels: res.timestamp,
-            datasets: [
-              {
-                fill: true,
-                // label: 'system_cpu_user_pct',
-                data: res.system_cpu_idle_pct,
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-                tension: 0.4,
-              },
-            ],
-          });
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          }
+      method: "GET",
+      url: "/idle_pct_data",
+    })
+      .then((response) => {
+        console.log(response);
+        const res = response.data;
+        set_idle_pct_data({
+          labels: res.timestamp,
+          datasets: [
+            {
+              fill: true,
+              // label: 'system_cpu_user_pct',
+              data: res.system_cpu_idle_pct,
+              borderColor: "rgb(53, 162, 235)",
+              backgroundColor: "rgba(53, 162, 235, 0.5)",
+              tension: 0.4,
+            },
+          ],
         });
-  
-      set_idle_pct_options({
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "System_cpu_idle_pct",
-          },
-        },
-        scales: {
-          y: {
-            suggestedMin: 0,
-            suggestedMax: 0.1,
-          },
-        },
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
       });
 
-      axios({
-        method: "GET",
-        url: "/iowait_pct_data",
-      })
-        .then((response) => {
-          console.log(response);
-          const res = response.data;
-          set_iowait_pct_data({
-            labels: res.timestamp,
-            datasets: [
-              {
-                fill: true,
-                // label: 'system_cpu_user_pct',
-                data: res.system_cpu_iowait_pct,
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-                tension: 0.4,
-              },
-            ],
-          });
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          }
+    set_idle_pct_options({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "System_cpu_idle_pct",
+        },
+      },
+      scales: {
+        y: {
+          suggestedMin: 0,
+          suggestedMax: 0.1,
+        },
+      },
+    });
+
+    axios({
+      method: "GET",
+      url: "/iowait_pct_data",
+    })
+      .then((response) => {
+        console.log(response);
+        const res = response.data;
+        set_iowait_pct_data({
+          labels: res.timestamp,
+          datasets: [
+            {
+              fill: true,
+              // label: 'system_cpu_user_pct',
+              data: res.system_cpu_iowait_pct,
+              borderColor: "rgb(53, 162, 235)",
+              backgroundColor: "rgba(53, 162, 235, 0.5)",
+              tension: 0.4,
+            },
+          ],
         });
-  
-      set_iowait_pct_options({
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "System_cpu_iowait_pct",
-          },
-        },
-        scales: {
-          y: {
-            suggestedMin: 0,
-            suggestedMax: 0.1,
-          },
-        },
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
       });
 
-      axios({
-        method: "GET",
-        url: "/softirq_pct_data",
-      })
-        .then((response) => {
-          console.log(response);
-          const res = response.data;
-          set_softirq_pct_data({
-            labels: res.timestamp,
-            datasets: [
-              {
-                fill: true,
-                // label: 'system_cpu_user_pct',
-                data: res.system_cpu_softirq_pct,
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-                tension: 0.4,
-              },
-            ],
-          });
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          }
+    set_iowait_pct_options({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "System_cpu_iowait_pct",
+        },
+      },
+      scales: {
+        y: {
+          suggestedMin: 0,
+          suggestedMax: 0.1,
+        },
+      },
+    });
+
+    axios({
+      method: "GET",
+      url: "/softirq_pct_data",
+    })
+      .then((response) => {
+        console.log(response);
+        const res = response.data;
+        set_softirq_pct_data({
+          labels: res.timestamp,
+          datasets: [
+            {
+              fill: true,
+              // label: 'system_cpu_user_pct',
+              data: res.system_cpu_softirq_pct,
+              borderColor: "rgb(53, 162, 235)",
+              backgroundColor: "rgba(53, 162, 235, 0.5)",
+              tension: 0.4,
+            },
+          ],
         });
-  
-      set_softirq_pct_options({
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "System_cpu_softirq_pct",
-          },
-        },
-        scales: {
-          y: {
-            suggestedMin: 0,
-            suggestedMax: 0.1,
-          },
-        },
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
       });
 
-      axios({
-        method: "GET",
-        url: "/total_pct_data",
-      })
-        .then((response) => {
-          console.log(response);
-          const res = response.data;
-          console.log(res.system_cpu_total_pct)
-          set_total_pct_data({
-            labels: res.timestamp,
-            datasets: [
-              {
-                fill: true,
-                // label: 'system_cpu_user_pct',
-                data: res.system_cpu_total_pct,
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-                tension: 0.4,
-              },
-            ],
-          });
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          }
+    set_softirq_pct_options({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "System_cpu_softirq_pct",
+        },
+      },
+      scales: {
+        y: {
+          suggestedMin: 0,
+          suggestedMax: 0.1,
+        },
+      },
+    });
+
+    axios({
+      method: "GET",
+      url: "/total_pct_data",
+    })
+      .then((response) => {
+        console.log(response);
+        const res = response.data;
+        console.log(res.system_cpu_total_pct)
+        set_total_pct_data({
+          labels: res.timestamp,
+          datasets: [
+            {
+              fill: true,
+              // label: 'system_cpu_user_pct',
+              data: res.system_cpu_total_pct,
+              borderColor: "rgb(53, 162, 235)",
+              backgroundColor: "rgba(53, 162, 235, 0.5)",
+              tension: 0.4,
+            },
+          ],
         });
-  
-      set_total_pct_options({
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "System_cpu_total_pct",
-          },
-        },
-        scales: {
-          y: {
-            suggestedMin: 0,
-            suggestedMax: 0.1,
-          },
-        },
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
       });
+
+    set_total_pct_options({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "System_cpu_total_pct",
+        },
+      },
+      scales: {
+        y: {
+          suggestedMin: 0,
+          suggestedMax: 0.1,
+        },
+      },
+    });
 
 
 
@@ -354,7 +342,7 @@ function Cpu() {
     <div className="Cpu">
       {/* <Sidebar /> */}
       <div className="Cpu-container">
-        {/* <Navbar /> */}
+        {/* <Navbar />
         <div className="rows">
           <div className="row">
             <div className="area-container">
@@ -378,7 +366,15 @@ function Cpu() {
               <Line options={total_pct_options} data={total_pct_data} />
             </div>
           </div>
-        </div>
+        </div> */}
+        <Line options={user_pct_options} data={user_pct_data} />
+        <Line options={idle_pct_options} data={idle_pct_data} />
+        <Line options={iowait_pct_options} data={iowait_pct_data} />
+        <Line options={system_pct_options} data={system_pct_data} />
+        <Line options={softirq_pct_options} data={softirq_pct_data} />
+
+        <Line options={total_pct_options} data={total_pct_data} />
+
       </div>
     </div>
   );
